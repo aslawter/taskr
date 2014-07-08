5bcf725c9d99609cf2bd4b18fb1bddb8f5f34edc
Andrew Slawter <aslawter@gmail.com>
2014-07-08 11:38:16 -0400

    Create validations to not allow empty entries

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,16 +6,22 @@ class TasksController < ApplicationController
   end
 
   def create
-    current_user.tasks.create(task_params)
-    redirect_to :tasks
-  end
+    @task = current_user.tasks.new(task_params)
+    @tasks = current_user.tasks
+
+    if @task.save 
+      redirect_to :tasks
+    else
+      render :index
+    end
+  end 
   
   private
  
- def task_params
+  def task_params
    params.require(:task).permit(
      :title,
      :body,
    )
- end 
+  end 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ActiveRecord::Base
+  validates :body, presence: true
+  validates :title, presence: true
 end
