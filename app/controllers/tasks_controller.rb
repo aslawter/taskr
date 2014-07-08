798254ba2ee2873a2159d0ede6aa0adb0ac3959b
Andrew Slawter <aslawter@gmail.com>
2014-07-08 11:06:24 -0400

    Users can create new tasks and have them displayed

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,21 @@
+class TasksController < ApplicationController
+  before_action :require_login
+  def index
+    @task = Task.new
+    @tasks = current_user.tasks
+  end
+
+  def create
+    current_user.tasks.create(task_params)
+    redirect_to :tasks
+  end
+  
+  private
+ 
+ def task_params
+   params.require(:task).permit(
+     :title,
+     :body,
+   )
+ end 
+end
